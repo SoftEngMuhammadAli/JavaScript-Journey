2af9c43efb74a7f3662d8e895131b79d4be6e9f0
Muhammad Ali <softeng.aliijaz@gmail.com>
2025-03-16 16:46:13 +0500

    javascript objects

---
+++ objects/js_objects.js
@@ -0,0 +1,31 @@
+let students = [
+  {
+    name: "muhammad ali",
+    roll_number: 22,
+    class: "12th",
+  },
+  {
+    name: "aisha khan",
+    roll_number: 23,
+    class: "11th",
+  },
+  {
+    name: "ahmed raza",
+    roll_number: 24,
+    class: "10th",
+  },
+  {
+    name: "fatima bibi",
+    roll_number: 25,
+    class: "9th",
+  },
+  {
+    name: "usman tariq",
+    roll_number: 26,
+    class: "8th",
+  },
+];
+
+console.log(students[0].class);
+
+console.log(students[2].roll_number);
